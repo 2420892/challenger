213f9d107bcc7727f86bc2b878260dd65c24b2c4
siyamanga22 <zwenisiyamanga@gmail.com>
2023-08-14 16:47:01 +0200

    wow

--- api/model/index.js
+++ api/model/index.js
@@ -3,9 +3,9 @@ const Orders =require('./Orders')
 const Books = require('./Books')
 const  BookAuthors = require('./BookAuthors')
 // EXPORT AL OBJJECTS
-    module.exports = {
-        Users: new Users(),
-        // Orders: new Orders(),
-        // Books : new Books(),
-        // BookAuthors :new BookAuthors()
-    }
+module.exports = {
+    users: new Users(),
+    // Orders: new Orders(),
+    // Books : new Books(),
+    // BookAuthors :new BookAuthors()
+}
--- api/model/users.js
+++ api/model/users.js
@@ -94,4 +94,4 @@ class Users{
         })
     }
 }
-module.exports = {Users}
+module.exports = Users
