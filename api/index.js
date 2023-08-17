92f0ec7664e64e01b080321a852152e1d8e1e7ff
siyamanga22 <zwenisiyamanga@gmail.com>
2023-08-17 14:29:34 +0200

    done

--- api/index.js
+++ api/index.js
@@ -2,21 +2,45 @@
 const {express, routes}= require ('./controller')
 const path =require('path')
 const app = express()
-const port = +process.eventNames.PORT||3000
+const cors = require('cors')
+const cookieParser = require("cookie-parser");
+ 
+const { errorHandling } = require("./middleware/ErrorHandling");
+const port = +process.env.PORT || 3301
 
-// Static 
-app.use(express.static('./static'))
+// Middleware - Application level
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Credentials", "true");
+    res.header("Access-Control-Allow-Methods", "*");
+    res.header("Access-Control-Request-Methods", "*");
+    res.header("Access-Control-Allow-Headers", "*");
+    res.header("Access-Control-Expose-Headers", "Authorization");
+    next();
+});
 app.use(
+    express.static('./static'),
+    // express.json(),
+    cookieParser(),
     express.urlencoded({
         extended:false
     }),
+    cors(),
     routes
 )
 routes.get('^/$|/challenger', (req, res)=>{
     res.sendFile(path.resolve(__dirname,
-        "../API/static/html/index.html"))
-}) 
-
+        "../api/static/html/index.html"))
+})
+// Handling all errors
+app.use(errorHandling);
+// Running a server
 app.listen(port,()=>{
     console.log(`The server is running on port ${port}`);
 })
+
+
+
+
+
+
--- api/middleware/authenticateuser.js
+++ api/middleware/authenticateuser.js
@@ -14,9 +14,25 @@ function createToken(user){
 
     )
 }
-module.exports = {
-    createToken
+// verify token
+function verifyAToken(req, res, next){
+    /*
+    To prevent undefined error, place ?. before your property.
+    */
+   try{
+        // Retrieve token from req.headers
+        console.log("Get token from req.headers['authorization']");
+        const token = req.headers["authorization"]
+        console.log(token);
+        next()
+   }catch(e){
+        res.json({
+            status: res.statusCode,
+            msg: e.message
+        })
+   }
 }
-// function verifyAToke(req,res,next){//we are placing our token on the cookies and ssigning a variable called autorisation
-//     const token = req.headers["authorization"].
-// }
+module.exports = {
+    createToken, verifyAToken
+}
+
